6b54b08b65f28e41490088fb1c0215f31f9e74d1
andyo <andy.olshansky@gmail.com>
2022-07-29 21:49:16 -0400

    commit

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-# Tutorial-Website
+## Tutorial-Website
 My first go at making a website following this tutorial https://www.youtube.com/watch?v=I2UBjN5ER4s
 I will be adding various frontend and backend features using React and Express respectively.
 Not meant to be treated as a finished product, mostly having fun and experimenting with different features.
+
+# TODO
+Make a page showing off services as well as pages to more information
+Make a dropdown menu for services by hovering over the button
+Make it possible to contact us
+Make it possible to sign up
+Make an email blast to anyone signed up
+Implement a youtube video into the site
+Add links to other sites
--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './components/pages/Home';
 import Services from './components/pages/Services';
-import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
                 <Routes>
                     <Route path='/' exact element={<Home/>}/>
                     <Route path='/services' element={<Services/>}/>
-                    <Route path='/products' element={<Products/>}/>
                     <Route path='/sign-up' element={<SignUp/>}/>
                 </Routes>
             </Router>
